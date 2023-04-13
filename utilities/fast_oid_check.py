d5f1f45d3da180880c2392ec965344df4497fcc7
reydan46 <andre-4667@mail.ru>
2023-04-13 16:57:30 +0300

    Reforged New New

--- utilities/fast_oid_check.py
+++ utilities/fast_oid_check.py
@@ -24,36 +24,46 @@ logger.addHandler(c_handler)
 
 
 def detect_type(ip_address, logger):
+    logger.info('oid.cisco_catalyst.mode_port')
     output, error = snmpwalk(oid.cisco_catalyst.mode_port, 'public', ip_address, 'Debug', logger=logger)
     if not error:
+        logger.info('oid.cisco_catalyst.native_port')
         output, error = snmpwalk(oid.cisco_catalyst.native_port, 'public', ip_address, 'Debug', logger=logger)
     if not error:
+        logger.info('oid.cisco_catalyst.hex_tag_port')
         output, error = snmpwalk(oid.cisco_catalyst.hex_tag_port, 'public', ip_address, 'Debug', logger=logger)
     if not error:
+        logger.info('oid.cisco_catalyst.untag_port')
         output, error = snmpwalk(oid.cisco_catalyst.untag_port, 'public', ip_address, 'Debug', logger=logger)
     if not error:
+        logger.info('oid.cisco_catalyst.hex_tag_noneg_port')
         output, error = snmpwalk(oid.cisco_catalyst.hex_tag_noneg_port, 'public', ip_address, 'Debug', logger=logger)
     if not error:
+        logger.info('oid.cisco_catalyst.port_channel_index_port')
         output, error = snmpwalk(oid.cisco_catalyst.port_channel_index_port, 'public', ip_address, 'Debug',
                                  logger=logger)
 
     if not error:
         return 'catalist'
 
+    logger.info('oid.cisco_sg.hex_tag_port')
     output, error = snmpwalk(oid.cisco_sg.hex_tag_port, 'public', ip_address, 'Debug', logger=logger)
     if not error:
+        logger.info('oid.cisco_sg.mode_port')
         output, error = snmpwalk(oid.cisco_sg.mode_port, 'public', ip_address, 'INDEX-INT', logger=logger)
 
     if not error:
         mode_port_states = [i[1] for i in output]
         if oid.cisco_sg.mode_port_state["cisco_sg_300"]["access"] in mode_port_states \
                 or oid.cisco_sg.mode_port_state["cisco_sg_300"]["tagged"] in mode_port_states:
+            logger.info('oid.cisco_sg.untag_port["cisco_sg_300"]')
             output, error = snmpwalk(oid.cisco_sg.untag_port["cisco_sg_300"], 'public', ip_address, 'Debug',
                                      logger=logger)
             if not error:
                 return 'SG300'
         elif oid.cisco_sg.mode_port_state["cisco_sg_350"]["access"] in mode_port_states \
                 or oid.cisco_sg.mode_port_state["cisco_sg_350"]["tagged"] in mode_port_states:
+            logger.info('oid.cisco_sg.untag_port["cisco_sg_350"]')
             output, error = snmpwalk(oid.cisco_sg.untag_port["cisco_sg_350"], 'public', ip_address, 'Debug',
                                      logger=logger)
             if not error:
