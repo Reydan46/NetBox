838c083de3f8e0724e77b7692f84c35ee16e10ef
reydan46 <andre-4667@mail.ru>
2023-04-14 08:27:59 +0300

    Reforged New New New

--- utilities/not_so_fast_check.py
+++ utilities/not_so_fast_check.py
@@ -24,52 +24,68 @@ c_format = logging.Formatter("[%(asctime)s.%(msecs)03d ] %(message)s", datefmt='
 c_handler.setFormatter(c_format)
 logger.addHandler(c_handler)
 
-
+len_line_print = 3
 def detect_type(ip_address, logger):
+    logger.info('Check: cisco_catalyst')
     logger.info('oid.cisco_catalyst.mode_port')
     output, error = snmpwalk(oid.cisco_catalyst.mode_port, 'public', ip_address, 'Debug', logger=logger)
     if not error:
+        logger.debug('Output:\n' + '\n'.join(output[:len_line_print])+'\n')
         logger.info('oid.cisco_catalyst.native_port')
         output, error = snmpwalk(oid.cisco_catalyst.native_port, 'public', ip_address, 'Debug', logger=logger)
     if not error:
+        logger.debug('Output:\n' + '\n'.join(output[:len_line_print])+'\n')
         logger.info('oid.cisco_catalyst.hex_tag_port')
         output, error = snmpwalk(oid.cisco_catalyst.hex_tag_port, 'public', ip_address, 'Debug', logger=logger)
     if not error:
+        logger.debug('Output:\n' + '\n'.join(output[:len_line_print])+'\n')
         logger.info('oid.cisco_catalyst.untag_port')
         output, error = snmpwalk(oid.cisco_catalyst.untag_port, 'public', ip_address, 'Debug', logger=logger)
     if not error:
+        logger.debug('Output:\n' + '\n'.join(output[:len_line_print])+'\n')
         logger.info('oid.cisco_catalyst.hex_tag_noneg_port')
         output, error = snmpwalk(oid.cisco_catalyst.hex_tag_noneg_port, 'public', ip_address, 'Debug', logger=logger)
     if not error:
+        logger.debug('Output:\n' + '\n'.join(output[:len_line_print])+'\n')
         logger.info('oid.cisco_catalyst.port_channel_index_port')
         output, error = snmpwalk(oid.cisco_catalyst.port_channel_index_port, 'public', ip_address, 'Debug',
                                  logger=logger)
 
     if not error:
+        logger.debug('Output:\n' + '\n'.join(output[:len_line_print])+'\n')
         return 'catalist'
+    logger.info('')
 
+    logger.info('Check: cisco_sg\n')
     logger.info('oid.cisco_sg.hex_tag_port')
     output, error = snmpwalk(oid.cisco_sg.hex_tag_port, 'public', ip_address, 'Debug', logger=logger)
     if not error:
+        logger.debug('Output:\n' + '\n'.join(output[:len_line_print])+'\n')
         logger.info('oid.cisco_sg.mode_port')
-        output, error = snmpwalk(oid.cisco_sg.mode_port, 'public', ip_address, 'INDEX-INT', logger=logger)
+        output, error = snmpwalk(oid.cisco_sg.mode_port, 'public', ip_address, 'Debug', logger=logger)
 
     if not error:
-        mode_port_states = [i[1] for i in output]
+        logger.debug('Output:\n' + '\n'.join(output[:len_line_print])+'\n')
+        mode_port_states = [i.split(":")[-1].strip() for i in output if ":" in i]
         if oid.cisco_sg.mode_port_state["cisco_sg_300"]["access"] in mode_port_states \
                 or oid.cisco_sg.mode_port_state["cisco_sg_300"]["tagged"] in mode_port_states:
+            logger.debug('Output:\n' + '\n'.join(output[:len_line_print])+'\n')
             logger.info('oid.cisco_sg.untag_port["cisco_sg_300"]')
             output, error = snmpwalk(oid.cisco_sg.untag_port["cisco_sg_300"], 'public', ip_address, 'Debug',
                                      logger=logger)
             if not error:
+                logger.debug('Output:\n' + '\n'.join(output[:len_line_print])+'\n')
                 return 'SG300'
         elif oid.cisco_sg.mode_port_state["cisco_sg_350"]["access"] in mode_port_states \
                 or oid.cisco_sg.mode_port_state["cisco_sg_350"]["tagged"] in mode_port_states:
+            logger.debug('Output:\n' + '\n'.join(output[:len_line_print])+'\n')
             logger.info('oid.cisco_sg.untag_port["cisco_sg_350"]')
             output, error = snmpwalk(oid.cisco_sg.untag_port["cisco_sg_350"], 'public', ip_address, 'Debug',
                                      logger=logger)
             if not error:
+                logger.debug('Output:\n' + '\n'.join(output[:len_line_print])+'\n')
                 return 'SG350'
+    logger.info('')
 
     return ''
 
