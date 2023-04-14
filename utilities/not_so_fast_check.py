d406a41f874d08ed0ee098acdf408b8c708af8d4
reydan46 <andre-4667@mail.ru>
2023-04-14 10:09:33 +0300

    Change color

--- utilities/not_so_fast_check.py
+++ utilities/not_so_fast_check.py
@@ -30,12 +30,13 @@ c_handler.setFormatter(c_format)
 logger.addHandler(c_handler)
 
 
-def red(text):
-    return f'{Fore.RED}{text}{Fore.RESET}'
+def fail(): return f'{Fore.LIGHTRED_EX}not passed{Fore.RESET}'
 
 
-def green(text):
-    return f'{Fore.GREEN}{text}{Fore.RESET}'
+def success(): return f'{Fore.LIGHTGREEN_EX}passed{Fore.RESET}'
+
+
+def oid_name(text): return f'{Fore.LIGHTCYAN_EX}{text}{Fore.RESET}'
 
 
 len_line_print = 3
@@ -49,123 +50,123 @@ def detect_type(ip_address, logger):
         ip_address = ip_address[2:]
         logger.info('Check: general')
         name_oid = 'oid.general.hostname'
-        logger.info(name_oid)
+        logger.info(oid_name(name_oid))
         output, error = snmpwalk(oid.general.hostname, 'public', ip_address, 'Debug', logger=logger,
                                  timeout_process=timeout_process)
-        result.append([name_oid, red('no passed') if error else green('passed')])
+        result.append([name_oid, fail() if error else success()])
         logger.debug('Output:\n' + '\n'.join(output[:len_line_print]) + '\n')
 
         name_oid = 'oid.general.model'
-        logger.info(name_oid)
+        logger.info(oid_name(name_oid))
         output, error = snmpwalk(oid.general.model, 'public', ip_address, 'Debug', logger=logger,
                                  timeout_process=timeout_process)
-        result.append([name_oid, red('no passed') if error else green('passed')])
+        result.append([name_oid, fail() if error else success()])
         logger.debug('Output:\n' + '\n'.join(output[:len_line_print]) + '\n')
 
         name_oid = 'oid.general.serial_number'
-        logger.info(name_oid)
+        logger.info(oid_name(name_oid))
         output, error = snmpwalk(oid.general.serial_number, 'public', ip_address, 'Debug', logger=logger,
                                  timeout_process=timeout_process)
-        result.append([name_oid, red('no passed') if error else green('passed')])
+        result.append([name_oid, fail() if error else success()])
         logger.debug('Output:\n' + '\n'.join(output[:len_line_print]) + '\n')
 
         name_oid = 'oid.general.svi_ip_addresses'
-        logger.info(name_oid)
+        logger.info(oid_name(name_oid))
         output, error = snmpwalk(oid.general.svi_ip_addresses, 'public', ip_address, 'Debug', logger=logger,
                                  timeout_process=timeout_process)
-        result.append([name_oid, red('no passed') if error else green('passed')])
+        result.append([name_oid, fail() if error else success()])
         logger.debug('Output:\n' + '\n'.join(output[:len_line_print]) + '\n')
 
         name_oid = 'oid.general.svi_masks'
-        logger.info(name_oid)
+        logger.info(oid_name(name_oid))
         output, error = snmpwalk(oid.general.svi_masks, 'public', ip_address, 'Debug', logger=logger,
                                  timeout_process=timeout_process)
-        result.append([name_oid, red('no passed') if error else green('passed')])
+        result.append([name_oid, fail() if error else success()])
         logger.debug('Output:\n' + '\n'.join(output[:len_line_print]) + '\n')
 
         name_oid = 'oid.general.svi_indexes'
-        logger.info(name_oid)
+        logger.info(oid_name(name_oid))
         output, error = snmpwalk(oid.general.svi_indexes, 'public', ip_address, 'Debug', logger=logger,
                                  timeout_process=timeout_process)
-        result.append([name_oid, red('no passed') if error else green('passed')])
+        result.append([name_oid, fail() if error else success()])
         logger.debug('Output:\n' + '\n'.join(output[:len_line_print]) + '\n')
 
         name_oid = 'oid.general.si_int_name'
-        logger.info(name_oid)
+        logger.info(oid_name(name_oid))
         output, error = snmpwalk(oid.general.si_int_name, 'public', ip_address, 'Debug', logger=logger,
                                  timeout_process=timeout_process)
-        result.append([name_oid, red('no passed') if error else green('passed')])
+        result.append([name_oid, fail() if error else success()])
         logger.debug('Output:\n' + '\n'.join(output[:len_line_print]) + '\n')
 
         name_oid = 'oid.general.si_mtu'
-        logger.info(name_oid)
+        logger.info(oid_name(name_oid))
         output, error = snmpwalk(oid.general.si_mtu, 'public', ip_address, 'Debug', logger=logger,
                                  timeout_process=timeout_process)
-        result.append([name_oid, red('no passed') if error else green('passed')])
+        result.append([name_oid, fail() if error else success()])
         logger.debug('Output:\n' + '\n'.join(output[:len_line_print]) + '\n')
 
         name_oid = 'oid.general.si_mac'
-        logger.info(name_oid)
+        logger.info(oid_name(name_oid))
         output, error = snmpwalk(oid.general.si_mac, 'public', ip_address, 'Debug', logger=logger,
                                  timeout_process=timeout_process)
-        result.append([name_oid, red('no passed') if error else green('passed')])
+        result.append([name_oid, fail() if error else success()])
         logger.debug('Output:\n' + '\n'.join(output[:len_line_print]) + '\n')
 
         name_oid = 'oid.general.si_description'
-        logger.info(name_oid)
+        logger.info(oid_name(name_oid))
         output, error = snmpwalk(oid.general.si_description, 'public', ip_address, 'Debug', logger=logger,
                                  timeout_process=timeout_process)
-        result.append([name_oid, red('no passed') if error else green('passed')])
+        result.append([name_oid, fail() if error else success()])
         logger.debug('Output:\n' + '\n'.join(output[:len_line_print]) + '\n')
 
     logger.info('Check: cisco_catalyst')
     name_oid = 'oid.cisco_catalyst.mode_port'
-    logger.info(name_oid)
+    logger.info(oid_name(name_oid))
     output, error = snmpwalk(oid.cisco_catalyst.mode_port, 'public', ip_address, 'Debug', logger=logger,
                              timeout_process=timeout_process)
-    result.append([name_oid, red('no passed') if error else green('passed')])
+    result.append([name_oid, fail() if error else success()])
 
     if not error:
         logger.debug('Output:\n' + '\n'.join(output[:len_line_print]) + '\n')
         name_oid = 'oid.cisco_catalyst.native_port'
-        logger.info(name_oid)
+        logger.info(oid_name(name_oid))
         output, error = snmpwalk(oid.cisco_catalyst.native_port, 'public', ip_address, 'Debug', logger=logger,
                                  timeout_process=timeout_process)
-        result.append([name_oid, red('no passed') if error else green('passed')])
+        result.append([name_oid, fail() if error else success()])
 
     if not error:
         logger.debug('Output:\n' + '\n'.join(output[:len_line_print]) + '\n')
         name_oid = 'oid.cisco_catalyst.hex_tag_port'
-        logger.info(name_oid)
+        logger.info(oid_name(name_oid))
         output, error = snmpwalk(oid.cisco_catalyst.hex_tag_port, 'public', ip_address, 'Debug', hex=True,
                                  logger=logger,
                                  timeout_process=timeout_process)
-        result.append([name_oid, red('no passed') if error else green('passed')])
+        result.append([name_oid, fail() if error else success()])
 
     if not error:
         logger.debug('Output:\n' + '\n'.join(output[:len_line_print]) + '\n')
         name_oid = 'oid.cisco_catalyst.untag_port'
-        logger.info(name_oid)
+        logger.info(oid_name(name_oid))
         output, error = snmpwalk(oid.cisco_catalyst.untag_port, 'public', ip_address, 'Debug', logger=logger,
                                  timeout_process=timeout_process)
-        result.append([name_oid, red('no passed') if error else green('passed')])
+        result.append([name_oid, fail() if error else success()])
 
     if not error:
         logger.debug('Output:\n' + '\n'.join(output[:len_line_print]) + '\n')
         name_oid = 'oid.cisco_catalyst.hex_tag_noneg_port'
-        logger.info(name_oid)
+        logger.info(oid_name(name_oid))
         output, error = snmpwalk(oid.cisco_catalyst.hex_tag_noneg_port, 'public', ip_address, 'Debug', hex=True,
                                  logger=logger,
                                  timeout_process=timeout_process)
-        result.append([name_oid, red('no passed') if error else green('passed')])
+        result.append([name_oid, fail() if error else success()])
 
     if not error:
         logger.debug('Output:\n' + '\n'.join(output[:len_line_print]) + '\n')
         name_oid = 'oid.cisco_catalyst.port_channel_index_port'
-        logger.info(name_oid)
+        logger.info(oid_name(name_oid))
         output, error = snmpwalk(oid.cisco_catalyst.port_channel_index_port, 'public', ip_address, 'Debug',
                                  logger=logger)
-        result.append([name_oid, red('no passed') if error else green('passed')])
+        result.append([name_oid, fail() if error else success()])
 
     if not error:
         logger.debug('Output:\n' + '\n'.join(output[:len_line_print]) + '\n')
@@ -174,18 +175,18 @@ def detect_type(ip_address, logger):
 
     logger.info('Check: cisco_sg\n')
     name_oid = 'oid.cisco_sg.hex_tag_port'
-    logger.info(name_oid)
+    logger.info(oid_name(name_oid))
     output, error = snmpwalk(oid.cisco_sg.hex_tag_port, 'public', ip_address, 'Debug', hex=True, logger=logger,
                              timeout_process=timeout_process)
-    result.append([name_oid, red('no passed') if error else green('passed')])
+    result.append([name_oid, fail() if error else success()])
 
     if not error:
         logger.debug('Output:\n' + '\n'.join(output[:len_line_print]) + '\n')
         name_oid = 'oid.cisco_sg.mode_port'
-        logger.info(name_oid)
+        logger.info(oid_name(name_oid))
         output, error = snmpwalk(oid.cisco_sg.mode_port, 'public', ip_address, 'Debug', logger=logger,
                                  timeout_process=timeout_process)
-        result.append([name_oid, red('no passed') if error else green('passed')])
+        result.append([name_oid, fail() if error else success()])
 
     if not error:
         logger.debug('Output:\n' + '\n'.join(output[:len_line_print]) + '\n')
@@ -194,10 +195,10 @@ def detect_type(ip_address, logger):
                 or oid.cisco_sg.mode_port_state["cisco_sg_300"]["tagged"] in mode_port_states:
             logger.debug('Output:\n' + '\n'.join(output[:len_line_print]) + '\n')
             name_oid = 'oid.cisco_sg.untag_port["cisco_sg_300"]'
-            logger.info(name_oid)
+            logger.info(oid_name(name_oid))
             output, error = snmpwalk(oid.cisco_sg.untag_port["cisco_sg_300"], 'public', ip_address, 'Debug',
                                      logger=logger)
-            result.append([name_oid, red('no passed') if error else green('passed')])
+            result.append([name_oid, fail() if error else success()])
 
             if not error:
                 logger.debug('Output:\n' + '\n'.join(output[:len_line_print]) + '\n')
@@ -206,10 +207,10 @@ def detect_type(ip_address, logger):
                 or oid.cisco_sg.mode_port_state["cisco_sg_350"]["tagged"] in mode_port_states:
             logger.debug('Output:\n' + '\n'.join(output[:len_line_print]) + '\n')
             name_oid = 'oid.cisco_sg.untag_port["cisco_sg_350"]'
-            logger.info(name_oid)
+            logger.info(oid_name(name_oid))
             output, error = snmpwalk(oid.cisco_sg.untag_port["cisco_sg_350"], 'public', ip_address, 'Debug',
                                      logger=logger)
-            result.append([name_oid, red('no passed') if error else green('passed')])
+            result.append([name_oid, fail() if error else success()])
 
             if not error:
                 logger.debug('Output:\n' + '\n'.join(output[:len_line_print]) + '\n')
@@ -223,13 +224,13 @@ while True:
     ip_address = input('Enter IP address: ')
 
     oidType, result = detect_type(ip_address, logger)
-    if oidType:
-        logger.info(f'Detected type: {oidType}')
-    else:
-        logger.error(f'No detected type!')
 
     table = PrettyTable()
     table.field_names = ["OID", "Result"]
     for line in result:
         table.add_row(line)
     logger.info(f"Check result:\n{table}")
+    if oidType:
+        logger.info(f'Detected type: {Fore.LIGHTYELLOW_EX}{oidType}{Fore.RESET}')
+    else:
+        logger.error(f'{Fore.RED}No detected type!{Fore.RESET}')
